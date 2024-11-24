20651a91e769d76c6558b4d2354f6b28c9c0671d
anbazhaganjr <88985534+anbazhaganjr@users.noreply.github.com>
2024-11-24 16:42:02 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
-    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwgHAifT13p5a21kest8TjARmPt4zAUOUAgFDfaa1gtwEqgTdmPqaU6E3HqBXqBFyHo/exec" // Replace with your Web App URL
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycby5-8oWzT0_CUVMSj49fPTeF6h-6DczB_X_eOZ5aSJx1pBY0pc85McO5f-gvXWsJDT8/exec" // Replace with your Web App URL
   };
   
